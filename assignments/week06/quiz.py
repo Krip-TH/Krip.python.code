78ab72edb62e06960fc0f5b78b5303f71e8c0f53
Krip Topongkasem <Krip.t@ku.th>
2025-10-13 11:14:58 +0700

    lll

--- assignments/week06/quiz.py
+++ assignments/week06/quiz.py
@@ -21,9 +21,9 @@ def calculate_circle(radius):
     pi = 3.14159
     area = pi * radius * radius
     circumference = 2 * pi * radius
-    return {
-        "area": round(area,2)
-        "circumference": round(circumference , 2)
+    return 
+    "area": round(area,2)
+    "circumference": round(circumference , 2)
 
 """ เขียน function ชื่อ create_user_profile ที่มีคุณสมบัติดังนี้:
 
